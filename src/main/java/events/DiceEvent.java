826b12d3b7b36c3a784b3b52f6fd58965c7c23dc
Jonatas <djonatas31@gmail.com>
2025-01-21 09:31:40 -0300

    feat: add set of trusted channels

--- src/main/java/events/DiceEvent.java
+++ src/main/java/events/DiceEvent.java
@@ -4,6 +4,7 @@ import commands.dice.Dice;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,14 +13,19 @@ public class DiceEvent extends ListenerAdapter {
   public void onMessageReceived(MessageReceivedEvent event) {
     if (event.getAuthor().isBot()) return;
 
-    String message = event.getMessage().getContentRaw();
+    Set<String> channels = Set.of("1331236965374230651", "1145746498475274301");
+    String channelId = event.getChannel().getId();
 
-    Pattern dice = Pattern.compile("d\\d+");
-    Matcher matcher = dice.matcher(message);
+    if (channels.contains(channelId)) {
+      String message = event.getMessage().getContentRaw();
 
-    if (matcher.find()) {
-      if (message.matches("^\\d*#?\\s*(\\d+d\\d+|\\d+)(\\s*[+\\-*/]\\s*(\\d+d\\d+|\\d+))*\\s*(\\s+[\\p{L}\\p{M}0-9\\- ]+)?$")) {
-        Dice.diceCommand(event);
+      Pattern dice = Pattern.compile("d\\d+");
+      Matcher matcher = dice.matcher(message);
+
+      if (matcher.find()) {
+        if (message.matches("^\\d*#?\\s*(\\d+d\\d+|\\d+)(\\s*[+\\-*/]\\s*(\\d+d\\d+|\\d+))*\\s*(\\s+[\\p{L}\\p{M}0-9\\- ]+)?$")) {
+          Dice.diceCommand(event);
+        }
       }
     }
   }
